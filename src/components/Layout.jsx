1a7f05363145bd3b676f2a9eeb98358ea4e14c02
bradcowwbyez-debug <bradcowwbyez@gmail.com>
2025-11-16 17:48:23 +0000

    fix(layout): hide Navbar and MobileNav on landing page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,24 +7,25 @@ import { useLocation } from 'react-router-dom'
 
 export default function Layout({ children, page, setPage, user, onLogout }) {
   const location = useLocation()
+  const isLanding = location?.pathname === '/' || location?.pathname === ''
   return (
     <div className="min-h-screen flex bg-[rgb(var(--bg))] text-[rgb(var(--text))]">
       {/* Dropdown menu replaces persistent sidebar */}
       {/* Sidebar removed per UX request; to keep menu accessible we use DropdownMenu in Navbar */}
 
       <div className="flex-1">
-        <Navbar user={user} setPage={setPage} />
+        {!isLanding && <Navbar user={user} setPage={setPage} />}
         <main className="pt-16 md:pt-20">
           <div className="w-full px-4 md:container-center">
             {children}
           </div>
         </main>
         {/* Only show footer on landing page */}
-        {location?.pathname === '/' && <Footer />}
+        {isLanding && <Footer />}
       </div>
 
-      {/* Mobile nav */}
-      <MobileNav page={page} setPage={setPage} user={user} onLogout={onLogout} />
+      {/* Mobile nav (hide on landing) */}
+      {!isLanding && <MobileNav page={page} setPage={setPage} user={user} onLogout={onLogout} />}
     </div>
   )
 }
